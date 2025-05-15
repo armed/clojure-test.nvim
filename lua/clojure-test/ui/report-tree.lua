ad2d111cbe6e8e2e6e6074328ea6c52f43694e25
Julien Vincent <m@julienvincent.io>
2025-05-15 11:39:03 +0100

    Sort tree nodes in order of failed/passed/pending

--- lua/clojure-test/ui/report-tree.lua
+++ lua/clojure-test/ui/report-tree.lua
@@ -74,7 +74,7 @@ local function assertion_to_node(test, assertion)
   return node
 end
 
-local function report_to_node(report)
+local function report_to_node(report, is_expanded)
   local report_line = report_to_line(report)
 
   local children = {}
@@ -85,19 +85,66 @@ local function report_to_node(report)
   local node = NuiTree.Node({
     line = report_line,
     test = report.test,
+    report = report,
   }, children)
 
-  if report.status == "failed" then
+  if report.status == "failed" or is_expanded then
     node:expand()
   end
   return node
 end
 
-local function reports_to_nodes(reports)
+local function reports_to_nodes(reports, prev_nodes)
+  local prev_nodes_by_test = {}
+  for _, node in ipairs(prev_nodes) do
+    prev_nodes_by_test[node.test] = node
+  end
+
   local nodes = {}
   for _, report in pairs(reports) do
-    table.insert(nodes, report_to_node(report))
+    local prev_node = prev_nodes_by_test[report.test]
+    local is_expanded = prev_node and prev_node:is_expanded()
+    table.insert(nodes, report_to_node(report, is_expanded))
   end
+
+  local curr_map = {}
+  for _, node in ipairs(nodes) do
+    curr_map[node.test] = node
+  end
+
+  local moved = { failed = {}, passed = {}, pending = {} }
+  local grouped = { failed = {}, passed = {}, pending = {} }
+
+  for _, prev in ipairs(prev_nodes) do
+    local current = curr_map[prev.test]
+    if current then
+      local prev_status = prev.report.status
+      local curr_status = current.report.status
+      if prev_status == curr_status then
+        table.insert(grouped[curr_status], current)
+      elseif prev_status == "pending" and (curr_status == "failed" or curr_status == "passed") then
+        table.insert(moved[curr_status], current)
+      else
+        table.insert(grouped[curr_status], current)
+      end
+      curr_map[prev.test] = nil
+    end
+  end
+
+  for _, node in pairs(curr_map) do
+    table.insert(grouped[node.report.status], node)
+  end
+
+  nodes = {}
+  for _, status in ipairs({ "failed", "passed", "pending" }) do
+    for _, n in ipairs(grouped[status]) do
+      table.insert(nodes, n)
+    end
+    for _, n in ipairs(moved[status]) do
+      table.insert(nodes, n)
+    end
+  end
+
   return nodes
 end
 
@@ -195,7 +242,7 @@ function M.create(window, on_event)
   end, {})
 
   function ReportTree:render_reports(reports)
-    tree:set_nodes(reports_to_nodes(reports))
+    tree:set_nodes(reports_to_nodes(reports, tree:get_nodes()))
     tree:render()
   end
 
